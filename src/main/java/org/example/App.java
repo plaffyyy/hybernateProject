4368489eba5d1e0d4bd6ff7a0e33cd8fedf8c2b3
plaffyyy <k.zimin@innopolis.university>
2025-01-22 20:49:09 +0300

    feat(init commit): structure and hibernate query example

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        session.beginTransaction();
+
+        Person person = session.get(Person.class, 1);
+        System.out.println(person.getName() + " " + person.getAge());
+
+        session.getTransaction().commit();
+
+        sessionFactory.close();
+
+    }
+}
